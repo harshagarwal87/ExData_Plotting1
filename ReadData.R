1cb5572cb2860204abc358b84cb4944c4406f4b9
harshagarwal87 <harshagarwal.kol.1987@gmail.com>
2020-09-13 08:40:59 +0530

    Typo fix

--- ReadData.R
+++ ReadData.R
@@ -6,6 +6,6 @@ hpc <- read.table("./household_power_consumption.txt",
 # Filtering for Dates = 1/2/2007 & 2/2/2007
 hpc <- filter(hpc, Date == "1/2/2007" | Date == "2/2/2007")
 
-# Creating a new column mergin Date & time and changing format of column to POSIXlt
+# Creating a new column merging Date & time and changing format of column to POSIXlt
 
 hpc$fullDate<- strptime(paste(hpc$Date, hpc$Time), format = "%d/%m/%Y %H:%M:%S")
